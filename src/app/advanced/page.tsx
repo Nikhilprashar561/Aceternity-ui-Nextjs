c0364369098bea2263e9f1c4c05dbec1c1c2aaf9
nikhilprashar561 <nikhilprashar@gmail.com>
2025-07-17 11:44:20 +0530

    music app

---
+++ src/app/advanced/page.tsx
@@ -0,0 +1,12 @@
+import { DotBackgroundDemo } from '@/components/Dot'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <DotBackgroundDemo />
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/basic/page.tsx
@@ -0,0 +1,12 @@
+import { BackgroundLinesDemo } from '@/components/Background'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+      <BackgroundLinesDemo />
+    </div>
+  )
+}
+
+export default page
